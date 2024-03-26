57a5da78d6269998bc45b38713c9e2288ba7201f
Pankaj <pkjdeveloper@gmail.com>
2024-03-26 15:33:46 +0530

    GetSafeNow Android

---
+++ library/external/span/build/generated/source/buildConfig/debug/me/gujun/android/span/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.gujun.android.span;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "me.gujun.android.span";
+  public static final String BUILD_TYPE = "debug";
+}
